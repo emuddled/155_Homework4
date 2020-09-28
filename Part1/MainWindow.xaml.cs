adc451100ce598aa256596d29924bcdde97edfb2
ersin <ersinclair@gmail.com>
2020-09-27 22:23:47 -0700

    Add project files.

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿/// Homework No. 4		Exercise Part 1
+/// File Name: BMIGUI
+/// @author: Evan Sinclair
+/// Date:  Sept 26, 2020
+///
+/// Problem Statement:Write a program that calculates a person’s BMI and prints a message telling
+/// whether they below, within, or above the healthy range.  Include a color element of blue (below), 
+/// green(within), yellow (above).Use a GUI for the input and output.Show the calculated BMI score in the GUI.
+/// 
+/// Overall Plan:
+/// 1)Create a canvas
+/// 2)Create text boxes and labels for weight and height
+/// 3)Create a text box for output
+/// 4)Create Button for execution
+/// 5)Calculate BMI
+/// 6)Output message and change color of canvas
+///
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // get input data from weight and height textboxes
+            
+            double weight = Convert.ToDouble(enterWeight.Text);
+            double height = Convert.ToDouble(enterHeight.Text);
+            
+            // calculate BMI
+            
+            double BMI = (weight * 720) / (height * height);
+            
+            // if else depending on BMI
+            if (BMI < 15)
+            {
+                mainCanvas.Background = new SolidColorBrush(Colors.Blue);
+                BMIOutput.Text = "Your BMI is: " + BMI + Environment.NewLine + "You are underweight.";
+            }
+            else if (BMI > 26)
+            {
+                mainCanvas.Background = new SolidColorBrush(Colors.Yellow);
+                BMIOutput.Text = "Your BMI is: " + BMI + Environment.NewLine + "You are overweight.";
+            }
+            else
+            {
+                mainCanvas.Background = new SolidColorBrush(Colors.Green);
+                BMIOutput.Text = "Your BMI is: " + BMI + Environment.NewLine + "You are within the healty range.";
+
+            }
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,72 @@
+﻿/// Homework No. 4		Exercise Part 2
+/// File Name: CoinFlip
+/// @author: Evan Sinclair
+/// Date:  Sept 26, 2020
+///
+/// Problem Statement:Write a program that will print out statistics for eight coin tosses. 
+/// The user will input either an “h” for heads or a “t” for tails for the eight tosses. The 
+/// program will then print out the total number and percentages of heads and tails.
+/// 
+/// Overall Plan:
+/// 1)declare int variables for heads, tails and total flips
+/// 2)give the user instructions for inputting coinflip results
+/// 3)use a for loop to get 8 inputs and increment heads or tails 
+/// 4)on an incorrect input, warn the user and decrement flips
+/// 5)calculate percent heads and tails
+/// 6)Output results
+///
+///
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables to increment
+            int heads = 0;
+            int tails = 0;
+            int flips;
+            
+            // char for input
+            char headOrTail;
+            
+            // double to calculate percentage heads/tails
+            double percentHeads;
+            
+            // for loop to read in flip results
+            Console.WriteLine("Welcome to the coin flip program");
+            Console.WriteLine("flip a coin 8 times and type in \"h\" for heads and \"t\" for tails after each flip");
+            for (flips = 0; flips < 8; flips++)
+            {
+                Console.WriteLine("flip number " + (flips + 1));
+                
+                headOrTail = Convert.ToChar(Console.ReadLine());
+                if (headOrTail == 'h')
+                {
+                    heads++;
+                }
+                else if (headOrTail == 't')
+                {
+                    tails++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, try again.");
+                    flips--;
+                }
+
+            }
+           
+            // calculate and display information
+            percentHeads = (Convert.ToDouble(heads) / 8) * 100;
+            Console.WriteLine("Number of heads: " + heads);
+            Console.WriteLine("Number of tails: " + tails);
+            Console.WriteLine("Percent heads: " + percentHeads);
+            Console.WriteLine("Percent tails: " + (100 - percentHeads));
+
+
+        }
+    }
+}
